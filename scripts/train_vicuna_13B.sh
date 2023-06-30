9f44118ef874dc104e54e0de5df1bd8ad09bb925
vbenara <t-vbenara@microsoft.com>
2023-06-30 08:22:27 +0000

    modify 13B script

---
+++ scripts/train_vicuna_13B.sh
@@ -0,0 +1,26 @@
+torchrun --nproc_per_node=8 --master_port=20001 fastchat/train/train_mem.py \
+    --model_name_or_path /workspace/weights/llama/13B \
+    --data_path /workspace/datasets/sharegpt/ShareGPT_V3_unfiltered_cleaned_split.json \
+    --bf16 True \
+    --output_dir output_vicuna_13B \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 32 \
+    --gradient_accumulation_steps 4 \
+    --evaluation_strategy "steps" \
+    --eval_steps 1500 \
+    --save_strategy "steps" \
+    --save_steps 1500 \
+    --save_total_limit 8 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.04 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap offload" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True
+
